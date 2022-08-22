95ab2fc480466caa689760da3eb6242359e24a08
jervi <gacembekhira@gmail.com>
2022-08-22 08:15:05 +0100

    register finished

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/global.scss',
+                'resources/css/register.scss',
+                'resources/css/login.scss',
+            ],
+            refresh: true,
+        }),
+    ],
+});
